75309ce84f66f3befd3aa1542e91bd3581055d6d
Oleksii Sokoliuk <104352184+singlefrequency@users.noreply.github.com>
2022-11-19 15:44:37 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 html_static_path = ['_static']
 html_logo = 'lancelot.svg'
 #html_css_files = 'custom.css'
