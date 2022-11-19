1fda70f0b1a9dbbefb199c194f80474f41c4d8af
Oleksii Sokoliuk <104352184+singlefrequency@users.noreply.github.com>
2022-11-19 14:50:14 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'LANCELOT'
+copyright = '2022, Sokoliuk'
+author = 'Sokoliuk'
 
 release = '0.1'
 version = '0.1.0'
