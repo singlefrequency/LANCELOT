796e6be006d48cc1f43d9fe42b93bf7120007b64
Oleksii Sokoliuk <104352184+singlefrequency@users.noreply.github.com>
2022-11-19 16:00:24 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,8 +16,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'furo'
+    'sphinx.ext.intersphinx'
 ]
 
 intersphinx_mapping = {
